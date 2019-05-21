0a0a2cc94f26d81fdfd2139ea33c6207dcf14316
onitatsuo <deus.no.eternity@gmail.com>
2019-05-21 15:01:53 +0900

    first commit

---
+++ packages.el
@@ -0,0 +1,9 @@
+(package! evil-cleverparens)
+(package! sly-repl-ansi-color)
+(package! evil-lisp-state)
+(package! magithub)
+(package! magit)
+(package! mozc)
+;(package! awesome-tab :recipe (:fetcher github :repo "manateelazycat/awesome-tab" :files ("*.el" "theme/*.png")))
+; above used for package add
+(package! evil-tabs)
